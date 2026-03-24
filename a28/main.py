b66b860ac9c6f2bd11a8b0cfce45ae816a745f59
Vanessa Roque <viroque19@gmail.com>
2026-03-24 13:20:05 -0700

    trickle charge tests complete

--- a28/main.py
+++ a28/main.py
@@ -3,20 +3,26 @@ import time
 import csv
 from datetime import datetime
 from maxusb_spmi import maxusb
+import glob
+import pandas as pd
+from icecream import ic 
 
-SPMI_SLAVE_ID = 0x03 #
-PATH=r'C:\Users\vroque\Programs\a28'
-CSV_HEADERS = ["timestamp", "ibatt", "vbatt", "floor", "ceiling"]
+CHG_IN = 15.5
+SPMI_SLAVE_ID = 0x03
+PATH=rf'C:\Users\vroque\Programs\a30\{CHG_IN}'
 
-ITRICKLE_VALS = [0.125, 0.466, 1, 1.225]
+CSV_HEADERS = ["timestamp", "v", "i", "vset", "ibatt", "vbatt", "floor", "ceiling"]
+FINAL_VALS = []
+ITRICKLE_VALS = [.125, .466, 1, 1.225]
 VTRICKLE_VALS = [3.1, 3.35]
-ITRICKLE_COUNT = 2
+ITRICKLE_COUNT = 4
 VTRICKLE_COUNT = 2
 CHARGE_VOLTAGE = 0 # 4.85, 5, 9, 15, 15.5
-CHARGER_VOLTAGES = [4.85, 5, 9, 15, 15.5]
+CHARGER_VOLTAGES = [5.1, 6.8, 9, 12, 15.5] # WCIN
+# CHARGER_VOLTAGES = [4.85, 5, 9, 15, 15.5]
 VTRICKLE_REGS = {
     3.1: 0x37,
-    3.35: 0x39
+    3.35: 0x3F
 }
 ITRICKLE_REGS = {
     .125: 0x0B,
@@ -37,47 +43,55 @@ def spmi_write(sid, addr, data):
 
 def setup_max77795():
     # ----------------------------
-    # SPMI setup for WCIN regulation
+    # SPMI setup trickle charge
     # ----------------------------
     spmi_write(0x3, 0x56, [0x0C])  # Unlock charger in cfg_6
     spmi_write(0x3, 0x54, [0x37])  # Termination Voltage = 4530mV cfg_4
-    spmi_write(0x3, 0x5A, [0x7F])  # Charge Current limit for CHGIN cfg_10
-    # spmi_write(0x3, 0x5A, [hex(wcin_ilim)])  # Charge Current limit for WCIN cfg_10
+    spmi_write(0x3, 0x59, [0xFF])  # Charge Current limit for CHGIN cfg_10
+    spmi_write(0x3, 0x5A, [0x7F])  # Charge Current limit for WCIN cfg_10
     spmi_write(0x3, 0x50, [0x05])  # Charge mode = 0x5
-    spmi_write(0x3, 0x52, [0x33])  # Max Out Fast Charging Current = 2.5A cfg_2
-    spmi_write(0x3, 0x67, [0x4E]) # enable inlim cont & dynamic floor cfg_23
+    spmi_write(0x3, 0x52, [0x1E])  # Increase Fast Charging Current = 1.5A cfg_2
+    spmi_write(0x3, 0x67, [0x0E]) # enable inlim cont & dynamic floor cfg_23 & low bandwidth
 
-CURRENT_JUMP = 1.5 # value where current jumps out of v trickle
+CURRENT_JUMP = 0.5 # value where current jumps out of v trickle
 
-def step_down(instruments, writer, VBATT: float = 3.5, target_itrickle: float = 0.125, target_vtrickle: float = 3.1):
+def step_down(instruments, writer, VBATT: float, target_itrickle: float = 0.125, target_vtrickle: float = 3.1):
     fluke, keithley = instruments
+    # set_battsim(keithley, VBATT)
     current_prev = read_keithley_2306(keithley)
-    set_battsim(keithley, VBATT)
     current = current_prev
+    vbat_prev = read_fluke(fluke)
+    # ic(VBATT)
     while abs(current - current_prev) < CURRENT_JUMP:
-        if VBATT > 2.9:
-            VBATT -= 0.1
-        elif VBATT > 2.8:
-            VBATT -=0.05
-        else: VBATT -= 0.01
+        # time.sleep(0.1)
+        # ic(VBATT)
+        print(VBATT)
         set_battsim(keithley, VBATT)
         current_prev = current
         current = read_keithley_2306(keithley)
         vbat = read_fluke(fluke)
+        if vbat > 3.2:
+            VBATT -= 0.01
+        else:
+            VBATT -= 0.002
         row = {
             "timestamp": datetime.now().strftime("%H:%M:%S.%f")[:-3],
+            "v": target_vtrickle,
+            "i": target_itrickle,
+            "vset": VBATT,
             "vbatt": vbat,
             "ibatt": current
         }
         if abs(current - current_prev) > CURRENT_JUMP:
-            print(f"Found hysteresis floor: {vbat}")
-            final_voltage = vbat
+            # print(f"Found hysteresis floor: {vbat}")
+            print(f"set: {VBATT}\nread: {vbat_prev}")
+            final_voltage = (vbat_prev, VBATT)
 
             row["floor"] = "true"
-        print(row)
+        # print(row)
         writer.writerow(row)
     # take three extra measurements after reaching the jump
-    for i in range(3):
+    for i in range(1):
         current = read_keithley_2306(keithley)
         vbat = read_fluke(fluke)
         row = {
@@ -88,36 +102,49 @@ def step_down(instruments, writer, VBATT: float = 3.5, target_itrickle: float = 
         }
         print(row)
         writer.writerow(row)
+        vbat_prev = vbat
+
     return final_voltage
 
 
 
-def step_up(instruments, writer, VBATT: float = 3.8, target_itrickle: float = 0.125, target_vtrickle: float = 3.1):
+def step_up(instruments, writer, VBATT: float = 2.9, target_itrickle: float = 0.125, target_vtrickle: float = 3.1):
     fluke, keithley = instruments
     current_prev = read_keithley_2306(keithley)
     current = current_prev
-    set_battsim(keithley, VBATT)
+    # set_battsim(keithley, VBATT)
+    vbat_prev = read_fluke(fluke)
     while abs(current - current_prev) < CURRENT_JUMP:
-        if VBATT < 3:
-            VBATT += 0.1
-        else:
-            VBATT += 0.01
+        print(VBATT)
         set_battsim(keithley, VBATT)
-        current = read_keithley_2306(keithley)
         vbat = read_fluke(fluke)
+        if vbat < target_vtrickle - 0.2:
+            VBATT += 0.005
+        else:
+            VBATT += 0.002
+
+        # ic(VBATT)
+        current_prev = current
+        current = read_keithley_2306(keithley)
         row = {
             "timestamp": datetime.now().strftime("%H:%M:%S.%f")[:-3],
+            "v": target_vtrickle,
+            "i": target_itrickle,
+            "vset": VBATT,
             "vbatt": vbat,
             "ibatt": current
         }
         if abs(current - current_prev) > CURRENT_JUMP:
-            final_voltage = vbat
-            print(f"Found hysteresis ceiling: {vbat}")
+            # print(f"Found hysteresis ceiling: {vbat_prev}")
+            final_voltage = (vbat_prev, VBATT)
             row["ceiling"] = "true"
-        print(row)
+            print(f"set: {VBATT}\nread: {vbat}")
+        # print(row)
         writer.writerow(row)
+        vbat_prev = vbat
+
     # take three extra measurements
-    for i in range(3):
+    for i in range(1):
         current = read_keithley_2306(keithley)
         vbat = read_fluke(fluke)
         row = {
@@ -128,28 +155,47 @@ def step_up(instruments, writer, VBATT: float = 3.8, target_itrickle: float = 0.
         }
         print(row)
         writer.writerow(row)
+        print(f"set: {VBATT}\nread: {vbat}")
+
+    row = {
+        "timestamp": "Change register values for next test"
+    }
+    writer.writerow(row)
+    vbat_prev = vbat
+
     return final_voltage
 
 
 def find_hysteresis(instruments: list[Instrument], charger_voltage):
-    if charger_voltage >= 5:
-        ITRICKLE_COUNT = 4
     fluke, keithley = instruments
-    set_battsim(keithley, 3.5)
+    set_battsim(keithley, 3.0)
     for v in range(VTRICKLE_COUNT):
         target_vtrickle = VTRICKLE_VALS[v]
-        spmi_write(0x3, 0x63, [VTRICKLE_REGS[target_vtrickle]])
+        spmi_write(0x3, 0x62, [VTRICKLE_REGS[target_vtrickle]])
+        print(f"Change V-Trickle to {VTRICKLE_VALS[v]}")
+
         for i in range(ITRICKLE_COUNT):
             target_itrickle = ITRICKLE_VALS[i]
             spmi_write(0x3, 0x63, [ITRICKLE_REGS[target_itrickle]])
-
-            filename = fr"{PATH}\A.32_{datetime.now().strftime(f'%Y%m%d_%H_%M_%S-{charger_voltage}-{target_vtrickle}V_{target_itrickle}A')}.csv"
+            print(f"Change I-Trickle to {ITRICKLE_VALS[i]}")
+            filename = fr"{PATH}\A.28_{datetime.now().strftime(f'%Y%m%d_%H_%M_%S-{charger_voltage}-{target_vtrickle}V_{target_itrickle}A')}.csv"
             with open(filename, "w", newline="") as f:
                 writer = csv.DictWriter(f, fieldnames=CSV_HEADERS)
                 writer.writeheader()
-                floor = step_down(instruments, writer, 3.8, target_itrickle)
-                ceiling = step_up(instruments, writer, floor, target_itrickle)
-                print(floor, ceiling)
+                vstart = target_vtrickle + 0.05   
+                ic(target_vtrickle, target_itrickle)   
+                ic(vstart)         
+                floor = step_down(instruments, writer, vstart, target_itrickle, target_vtrickle)
+                ic(floor)
+                step_up_start = floor[1]
+                step_up_start += 0.15
+                if target_itrickle >= 0.466:
+                    step_up_start -= 0.05
+                ceiling = step_up(instruments, writer, step_up_start, target_itrickle, target_vtrickle)
+                print(f"floor, ceiling: {floor[0], ceiling[0]}")
+                floor_round = round(floor[0], 3)
+                FINAL_VALS.append((floor_round, ceiling[0]))
+    ic(f"Final Vals: {FINAL_VALS}")
 
             
 
@@ -158,23 +204,24 @@ def main():
     instruments = setup_instruments()
     setup_max77795()
     fluke, keithley = instruments
-    print("Input the charge voltage you want to test (4.85, 5, 9, 15, 15.5)\n\n")
+    print(f"Input the charge voltage you want to test {CHARGER_VOLTAGES}\n\n")
     CHARGE_VOLTAGE = float(input("Voltage: "))
     if(CHARGE_VOLTAGE not in CHARGER_VOLTAGES):
         CHARGE_VOLTAGE = int(input("Voltage: "))
+    ic(CHARGE_VOLTAGE)
     find_hysteresis(instruments=instruments, charger_voltage=CHARGE_VOLTAGE)
-    # target_vtrickle = VTRICKLE_VALS[0]
-    # target_itrickle = ITRICKLE_VALS[0]
-    # filename = fr"{PATH}\A.28_{datetime.now().strftime(f'%Y%m%d_%H_%M_%S-{target_vtrickle}V_{target_itrickle}A')}.csv"
-    # with open(filename, "w", newline="") as f:
-    #     writer = csv.DictWriter(f, fieldnames=CSV_HEADERS)
-    #     writer.writeheader()
-    #     floor = step_down(instruments, writer, 3.8, target_itrickle, target_vtrickle)
-    #     print(floor)
+    charger_voltage = CHARGE_VOLTAGE
+        # Merge all CSVs for this charger voltage into one master file
+    master_filename = fr"{PATH}\A.28_DONE_{charger_voltage}V_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+    csv_files = glob.glob(fr"{PATH}\A.28_*-{charger_voltage}-*.csv")
+    dfs = [pd.read_csv(f) for f in csv_files]
+    if dfs:
+        pd.concat(dfs, ignore_index=True).to_csv(master_filename, index=False)
+        print(f"Master CSV saved: {master_filename}")
 
 
 
-    # for instrument in instruments:
-    #     instrument.close()
+    for instrument in instruments:
+        instrument.close()
 if __name__ == "__main__":
     main()
